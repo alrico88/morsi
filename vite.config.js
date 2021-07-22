83982950512a126c99274b14a2f54bb0f286178b
Alberto Rico <alrico88@gmail.com>
2021-07-22 22:44:56 +0200

    📦 PWA Support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [reactRefresh(), VitePWA({
+    strategies: 'injectManifest',
+    manifest: {
+      name: 'morssi',
+      short_name: 'morsi',
+      theme_color: '#4995f6',
+      background_color: '#4995f6',
+      display: 'minimal-ui',
+      scope: '/',
+      start_url: '/',
+      icons: [
+        {
+          src: '/pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: '/pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+      ],
+    },
+  })],
 });
