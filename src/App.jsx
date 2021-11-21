0ace920d08b1028a51e3b3ac6abb59d4c2d85af5
Alberto Rico <alrico88@gmail.com>
2021-11-21 18:06:08 +0100

    feature: offline support

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {
   Container, Row, Col,
 } from 'react-bootstrap';
 import './App.scss';
+import { useRegisterSW } from 'virtual:pwa-register/react';
 import Encoded from './components/Encoded';
 import PlaybackControl from './components/PlaybackControl';
 import HeaderTitle from './components/HeaderTitle';
@@ -12,6 +13,22 @@ import ReloadPrompt from './components/ReloadPrompt';
 
 function App() {
   const [text, setText] = useState('');
+
+  const { updateServiceWorker } = useRegisterSW({
+    onRegistered(r) {
+      // eslint-disable-next-line prefer-template
+      console.log('SW Registered: ' + r);
+    },
+    onRegisterError(error) {
+      console.log('SW registration error', error);
+    },
+    onOfflineReady() {
+      console.log('Ready to work offline');
+    },
+    onNeedRefresh() {
+      updateServiceWorker(true);
+    },
+  });
 
   return (
     <>
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,31 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   plugins: [
     VitePWA({
-      base: '/',
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'morsi',
+        short_name: 'morsi',
+        description: 'Encode and playback texts in Morse Code',
+        theme_color: '#2952cc',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
     }),
     reactRefresh(),
   ],
